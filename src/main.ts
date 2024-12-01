9ac68e3692e4274d65c7bd0780f86da3d43f2881
mammaadDeveloper <developerMammad@proton.me>
2024-12-01 15:50:04 +0330

    feat(doc): Install swagger to project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
@@ -14,6 +15,16 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Restaurant API')
+    .setDescription('The Nest Restaurant API description')
+    .setVersion('0.0')
+    .addTag('menu, order, payment')
+    .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
 
   app.enableVersioning({
     type: VersioningType.URI
